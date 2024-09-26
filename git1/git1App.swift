71c72204bb2b0eb541f85e69441d5d674ef2e0ff
nke <nke>
2024-09-26 12:00:56 +0700

    Initial Commit

---
+++ git1/git1App.swift
@@ -0,0 +1,17 @@
+//
+//  git1App.swift
+//  git1
+//
+//  Created by user on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct git1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
